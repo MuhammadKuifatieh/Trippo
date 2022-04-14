2e469ee8057db88ca457832479c27ec98f20cf9f
muhammad_kuifatieh <muhammad.kuifatieh@gmail.com>
2022-04-15 02:00:34 +0300

    add localization , injection and app router

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'core/theme_app.dart';
-import 'features/explore/presentation/pages/explore_screen.dart';
-import 'features/home/presentation/pages/home_screen.dart';
 import 'features/main/presentation/pages/main_screen.dart';
-import 'features/plan/presentation/pages/plan_screen.dart';
-import 'features/profile/presentation/pages/profile_screen.dart';
+import 'injection.dart' as di;
+import 'router/app_router.dart';
 
-void main() {
+void main() async {
+  di.init();
   runApp(const MyApp());
 }
 
@@ -17,15 +18,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: const Locale('en'),
+      supportedLocales: const [
+        Locale('ar', ''),
+        Locale('en', ''),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        AppLocalizations.delegate,
+      ],
       title: 'Trippo',
       theme: ThemeApp().getTheme(),
       home: const MainScreen(),
-      routes: {
-        HomeScreen.routeName: (context) => const HomeScreen(),
-        PlanScreen.routeName: (context) => const PlanScreen(),
-        ExploreScreen.routeName: (context) => const ExploreScreen(),
-        ProfileScreenn.routeName: (context) => const ProfileScreenn(),
-      },
+      onGenerateRoute: AppRouter.onGenerateRoute,
     );
   }
 }
